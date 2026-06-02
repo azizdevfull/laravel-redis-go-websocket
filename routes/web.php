cd7f55c65fd581a03b4b8c4ff82e8bdc9fabb1f7
Azizbek Isroilov <aziz16110904@gmail.com>
2026-06-02 10:22:01 +0500

    add MessageSent broadcast event with demo route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Events\MessageSent;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    MessageSent::dispatch('Hello, world!');
     return view('welcome');
 });
